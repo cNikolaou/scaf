9e785d093ef1f7dc3b7292a892ca4c8fbb800951
Christos Nikolaou <me@christosnikolaou.org>
2023-07-01 13:48:41 +0100

    Add func to check when local network is running
    Add functions to check when the local network is up and running
    and the transcation block count can be received by the RPC call.

--- src/network.ts
+++ src/network.ts
@@ -1,8 +1,11 @@
 import { execSync, spawn, ChildProcess } from 'child_process';
+import http from 'http';
 
 const networkRunCommand = 'start --network.config sui_local_network/network.yaml'.split(' ');
 const networkResetCommand =
     'genesis --from-config genesis.yaml --working-dir sui_local_network/ -f'.split(' ');
+
+const CHECK_NETWORK_UP_INTERVAL = 2000;
 
 export class Network {
     networkResetProcess: ChildProcess | null = null;
@@ -60,6 +63,73 @@ export class Network {
     public stopNetwork() {
         if (this.networkRunProcess !== null) {
             this.networkRunProcess.kill();
+        }
+    }
+
+    private localnetTranscationsBlocksRpc(): Promise<string> {
+        // Make an RPC call to get the total block count and return
+        // a promise that will either resolve in a string with the
+        // serialised JSON response or will be rejected if there is
+        // an issue with the request
+
+        const data = JSON.stringify({
+            jsonrpc: '2.0',
+            id: 1,
+            method: 'sui_getTotalTransactionBlocks',
+            params: [],
+        });
+
+        const options = {
+            method: 'POST',
+            hostname: '127.0.0.1',
+            port: 9000,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        return new Promise((resolve, reject) => {
+            const req = http.request(options, (res) => {
+                let data = '';
+
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                res.on('end', () => {
+                    resolve(data);
+                });
+            });
+
+            req.on('error', (error) => {
+                reject(error);
+            });
+
+            req.write(data);
+            req.end();
+        });
+    }
+
+    public async transactionBlocksCount() {
+        // Get the transaction blocks count or -1 if there is an
+        // issue with the RPC request to the network; -1 is used to
+        // easily distinguish between a successful RPC call, which
+        // should return a natural number and an error that occured
+
+        try {
+            const data = await this.localnetTranscationsBlocksRpc();
+            return JSON.parse(data);
+        } catch (error) {
+            return -1;
+        }
+    }
+
+    public async waitUntilNetworkRuns() {
+        // Wait loop until the network starts and returns the total
+        // number of transaction blocks which should be a natural number
+
+        while ((await this.transactionBlocksCount()) === -1) {
+            await sleepForMs(CHECK_NETWORK_UP_INTERVAL);
         }
     }
 }
