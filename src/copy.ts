55dcfcfdde4c88513e599cf793ee5d1c56a408a6
Christos Nikolaou <me@christosnikolaou.org>
2023-07-02 19:35:46 +0100

    Update copy to copy only non-existing files/dirs
    Update the `copy.js` function to copy only the non-existing
    files or directories from the `examples` directory.

--- src/copy.ts
+++ src/copy.ts
@@ -4,20 +4,37 @@ import fs from 'fs';
 import path from 'path';
 import fse from 'fs-extra';
 
-async function copyDirectory(examplesDir: string, toDir: string) {
-    // create target directory if it doesn't exist
+async function copyDirectory(fromDir: string, toDir: string) {
+    // Copy the files that do not exist in `toDir` and they are in `fromDir`
+
+    // Create target directory if it doesn't exist
     if (!fs.existsSync(toDir)) {
         fs.mkdirSync(toDir);
     }
+
+    // Get list of files and directories
+    const filesAndDirs = await fse.readdir(fromDir);
+
     try {
-        await fse.copy(examplesDir, toDir, { overwrite: false, errorOnExist: true });
+        for (const fd of filesAndDirs) {
+            const src = path.join(fromDir, fd);
+            const dst = path.join(toDir, fd);
+
+            // Check if the file or dir exists and copy the files if it doesn't
+            if (!(await fse.pathExists(dst))) {
+                await fse.copy(src, dst, { overwrite: false, errorOnExist: true });
+            }
+        }
         console.log(`Example project created under directory "${path.resolve(toDir)}/"`);
     } catch (error) {
-        console.log(`Copy failed as the directory "${path.resolve(toDir)}" is not empty`);
+        console.log(`Copy to directory "${path.resolve(toDir)}" failed`);
+        console.log(error);
     }
 }
 
+const examplesDir = path.join(__dirname, '../examples');
 const toDir = process.argv[2] || './';
-copyDirectory('./examples', toDir).catch((err) => {
+
+copyDirectory(examplesDir, toDir).catch((err) => {
     console.log(err);
 });
